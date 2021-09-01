df0cf7a6386e8848a6a947d6104bc10d2f1f7e23
Thiago da Silva <thiagodasilvag1989@gmail.com>
2021-08-31 23:03:25 -0300

    Commit Inicial API

---
+++ APICatalogoJogosDIO/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogoJogosDIO.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException() : base("Este Jogo Não Está Cadastrado") { }
+    }
+}
